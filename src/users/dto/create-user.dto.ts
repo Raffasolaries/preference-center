0fc9455aeb73b31b343826a043a363820f840866
Raffasolaries <raffasolaries@gmail.com>
2021-03-21 20:33:15 +0100

    repo init

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,30 @@
+import { IsEmail, IsNotEmpty, IsString, IsDefined, IsArray, ValidateNested, IsOptional, IsEnum, IsBoolean } from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateEventDto } from '../../events/dto/create-event.dto';
+
+enum ConsentType {
+ email_notification = 'email_notification',
+ sms_notification = 'sms_notification'
+}
+
+class Consent {
+ @IsDefined()
+ @IsEnum(ConsentType)
+	id: string;
+
+ @IsDefined()
+ @IsBoolean()
+ enabled: boolean;
+}
+
+export class CreateUserDto {
+ @IsEmail()
+ @IsNotEmpty()
+ email: string;
+
+ @IsArray()
+ @ValidateNested({ each: true })
+	@Type(() => Consent)
+	@IsOptional()
+ consents?: Consent[]
+}
