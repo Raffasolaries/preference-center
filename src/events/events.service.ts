8bd0d2099b255e91c3f4c7df0df3adcd2fa2f1ce
Raffasolaries <raffasolaries@gmail.com>
2021-03-28 23:00:30 +0200

    events service udpate

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -25,7 +25,34 @@ export class EventsService {
   // }
 
   public async update(dto: EventDTO) {
-   return await this.repo.update(dto.user.id, { consents: [...dto.consents] });
+   const found = await this.repo.find({ where: { id: dto.user.id }});
+   console.log('found user', found);
+   const foundConsents = [];
+   let foundEmailNotificationsConsent = found[0].consents && found[0].consents.length > 0 ? found[0].consents.filter(consent => consent.id === 'email_notifications') : [];
+   let foundSmsNotificationsConsent = found[0].consents && found[0].consents.length > 0 ? found[0].consents.filter(consent => consent.id === 'sms_notifications') : [];
+   let newEmailNotificationsConsent = dto.consents && dto.consents.length > 0 ? dto.consents.filter(consent => consent.id === 'email_notifications') : [];
+   let newSmsNotificationsConsent = dto.consents && dto.consents.length > 0 ? dto.consents.filter(consent => consent.id === 'sms_notifications') : [];
+   const newConsents = [];
+   if (foundEmailNotificationsConsent.length > 0 && newEmailNotificationsConsent.length > 0) {
+    newConsents.push(newEmailNotificationsConsent[0]);
+   }
+   if (foundEmailNotificationsConsent.length > 0 && newEmailNotificationsConsent.length === 0) {
+    newConsents.push(foundEmailNotificationsConsent[0]);
+   } 
+   if (foundSmsNotificationsConsent.length > 0 && newSmsNotificationsConsent.length > 0) {
+    newConsents.push(newSmsNotificationsConsent[0]);
+   } 
+   if (foundSmsNotificationsConsent.length > 0 && newSmsNotificationsConsent.length === 0) {
+    newConsents.push(foundSmsNotificationsConsent[0]);
+   }
+   if (foundEmailNotificationsConsent.length === 0 && newEmailNotificationsConsent.length > 0) {
+    newConsents.push(newEmailNotificationsConsent[0]);
+   }
+   if (foundSmsNotificationsConsent.length === 0 && newSmsNotificationsConsent.length > 0) {
+    newConsents.push(newSmsNotificationsConsent[0]);
+   }
+   console.log('newConsents', newConsents);
+   return await this.repo.update(dto.user.id, { consents: newConsents });
   }
 
   // remove(id: number) {
